e2d190ab9c4e5aa3837c686e94b5587d49afa386
Andries Nieuwenhuize <nieu0082@hz.nl>
2023-02-01 21:09:24 +0100

    algabraic notation to technical grid coordinates

--- engine/board.js
+++ engine/board.js
@@ -9,14 +9,32 @@ const grid = [
     ['', '', '', '', '', '', '', ''] // 8th rank
 ];
 
+const pieces = {
+    king: 'K', queen: 'Q', bishop: 'B', knight: 'N', pawn: 'p'
+}
+
 const w_captures = [], 
       b_captures = [];
 
 const squares = {
     
         grid,
-        count: () => 8 * 8    
+        count: () => grid.reduce((sum, rank) => sum + rank.length, 0)    
 }
 
+// Example: e4 -> { rank: 3, file: 4 }
+const algabraicToGridCoords = (loc) => {
+    let coord = [...loc];
+    let f = "abcdefgh".indexOf(coord[0]);
+    let r = parseInt(coord[1]) -1;
 
-export { squares };
+    return { rank: r, file: f }
+}
+
+const initGame = () => {
+
+
+};
+
+
+export { squares, algabraicToGridCoords };
--- test/board.test.js
+++ test/board.test.js
@@ -1,5 +1,12 @@
-import * as board from '../engine/board'
+import { squares, algabraicToGridCoords } from '../engine/board'
 
 test('The board contains 64 squares', () => {
-    expect(board.squares.count()).toBe(64)
+    expect(squares.count()).toBe(64)
+})
+
+// internal test
+test('The technical board accepts e4 as file: 4, rank: 3', () => {
+    let coords1 = algabraicToGridCoords("e4");
+    expect(coords1.file).toBe(4)
+    expect(coords1.rank).toBe(3)
 })
