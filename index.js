f0d0dd6b3e3d94929db56a40cd40a83b210cd1dd
Andries Nieuwenhuize <a.nieuwenhuize@hz.nl>
2023-02-02 11:37:15 +0100

    readline-promise added

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 import { squares, newGame } from './engine/board.js'
 import * as view from './cli/view.js'
 
+import * as readline from 'readline-promise';
+
 
 console.log('Welcome to De Knol.');
 console.log('The chess engine with four legs solid on the ground.');
